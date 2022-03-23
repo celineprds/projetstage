c932fae501f5283bb6aeb07a0cb71484e2e094dc
prade <prade@Pc-de-Celine.home>
2022-03-23 16:07:09 +0100

    Phase 3 : introduction à Twig et simplification du contrôleur

--- src/Controller/AcceuilController.php
+++ src/Controller/AcceuilController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,12 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
  *
  * @author prade
  */
-class AcceuilController {
+class AcceuilController extends AbstractController{
+    
     /**
      * @Route("/", name="acceuil")
      * @return Response
      */
     public function index(): Response{
-            return new Response('Hello World !');
+            return $this->render("pages/acceuil.html.twig");
     }
 }
